e406d2529cd587639d9c5d54b6dfef6b7bb09821
gregor mihelac <alternativa.rocks@gmail.com>
2024-02-29 17:56:23 +0100

    new cherry blossoms

--- assets/js/cherry-blossom.js
+++ assets/js/cherry-blossom.js
@@ -8,9 +8,10 @@ const ctx = canvas.getContext('2d');
 // Create an array to store the cherry blossom leaves
 const leaves = [];
 
-// Set canvas size to cover half of the screen initially
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// Set canvas size to cover 99.95% of the window
+// this is due to the fact that the canvas will overflow the window if it's set to 100%
+canvas.width = window.innerWidth * 0.9995;
+canvas.height = window.innerHeight * 0.9995;
 
 class Leaf {
     constructor(x, y, size, speed) {
@@ -62,6 +63,47 @@ function animate() {
 
     requestAnimationFrame(animate);
 }
+
+// Function that scrolls down the website after a delay
+function scrollDownSmoothly() {
+    const scrollHeight = window.innerHeight / 2;
+    const duration = 9000; // 9 seconds
+    const delay = 3000; // 3 seconds
+
+    let isUserScrolling = false;
+
+    // This is an attempt at smooth scrolling in js
+    setTimeout(() => {
+        const startTime = performance.now();
+        const startY = window.pageYOffset;
+        const endY = startY + scrollHeight;
+
+        function scrollStep(timestamp) {
+            if (isUserScrolling) {
+                return; // Exit the function if the user scrolls on their own
+            }
+
+            const currentTime = timestamp - startTime;
+            const progress = currentTime / duration;
+
+            const easeInOutCubic = t => t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+            const easedProgress = easeInOutCubic(progress);
+
+            window.scrollTo(0, startY + (endY - startY) * easedProgress);
+
+            if (currentTime < duration) {
+                requestAnimationFrame(scrollStep);
+            }
+        }
+
+        requestAnimationFrame(scrollStep);
+    }, delay);
+}
+
+// Executing scrollDownSmoothly function only once at the startup of the page
+window.addEventListener('DOMContentLoaded', () => {
+    scrollDownSmoothly();
+});
 
 createLeaves();
 animate();
