1b9485cbe3ff6e58b19cd79f32ec64585dbb06e2
gregor mihelac <alternativa.rocks@gmail.com>
2024-03-02 22:27:54 +0100

    more updates

--- assets/js/game_of_life_sim.js
+++ assets/js/game_of_life_sim.js
@@ -5,8 +5,8 @@ document.body.appendChild(canvas);
 // get the 2D rendering context
 const ctx = canvas.getContext('2d');
 
-const numBlocksX = 700;
-const numBlocksY = 700;
+const numBlocksX = 200;
+const numBlocksY = 200;
 
 // calculate the maximum dimension of the display
 const maxDimension = Math.min(window.innerWidth, window.innerHeight);
