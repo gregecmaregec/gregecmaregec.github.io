60019716d1f7659a52654d31b5bab49d471e07f2
gregor mihelac <alternativa.rocks@gmail.com>
2024-03-07 16:44:47 +0100

    updating text interface

--- assets/js/text_interface.js
+++ assets/js/text_interface.js
@@ -6,14 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   inputField.addEventListener('input', function () {
     const userInput = inputField.value;
-    fetch(`https://gmserver.xyz?input=${userInput}`)
-      .then(response => response.text())
-      .then(data => {
-        outputField.innerText = data;
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+    outputField.innerText = userInput;
   });
 });
-  
