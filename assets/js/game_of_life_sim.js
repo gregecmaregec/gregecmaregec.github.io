45511d807cbda5eedab21471569688fdc1eadba7
gregor mihelac <alternativa.rocks@gmail.com>
2024-03-07 22:23:37 +0100

    more updates

--- assets/js/game_of_life_sim.js
+++ assets/js/game_of_life_sim.js
@@ -124,7 +124,7 @@ function drawGrid() {
             const isAlive = grid[x][y] === 1;
 
             if (isAlive) {
-                ctx.fillStyle = 'rgba(106, 0, 55, 0.8)'; // red color with 60% opacity
+                ctx.fillStyle = 'rgba(156, 0, 55, 0.8)'; // red color with 60% opacity
             } else {
                 ctx.fillStyle = 'rgba(0, 0, 0, 0)'; // completely transparent
             }
