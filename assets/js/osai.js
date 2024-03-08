5fbee307e0b7ff67faf66e36878abcbca2075345
gregor mihelac <alternativa.rocks@gmail.com>
2024-03-08 11:14:05 +0100

    more changes

--- assets/js/osai.js
+++ assets/js/osai.js
@@ -1,44 +1,48 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const inputField = document.getElementById('inputField');
-  const outputField = document.getElementById('outputField');
-  const sendButton = document.getElementById('sendButton');
-
-  outputField.innerText = 'Hello World!'; // Add this line
-
-  inputField.addEventListener('input', function () {
-    if (this.value.length > 0) {
-      sendButton.style.display = 'block';
-    } else {
+document.addEventListener('DOMContentLoaded', function (
+  ) {
+    const inputField = document.getElementById('inputField');
+    const outputField = document.getElementById('outputField');
+    const sendButton = document.getElementById('sendButton');
+  
+    outputField.innerText = 'Hello World!';
+  
+    inputField.addEventListener('input', function (
+  ) {
+      if (this.value.length > 0) {
+        sendButton.style.display = 'block';
+      } else {
+        sendButton.style.display = 'none';
+      }
+    });
+  
+    inputField.addEventListener('keypress', function (e) {
+      if (e.key === 'Enter') {
+        transmitData();
+      }
+    });
+  
+    sendButton.addEventListener('click', function (
+  ) {
+      transmitData();
+    });
+  
+    function transmitData(
+  ) {
+      if (inputField.value.length > 0) {
+        let newContent = document.createElement('div');
+        newContent.textContent = inputField.value;
+        newContent.classList.add('message');
+        outputField.appendChild(newContent);
+        scrollToBottom();
+      }
+  
+      inputField.value = '';
       sendButton.style.display = 'none';
     }
-  });
-
-  inputField.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-      transmitData();
+  
+    function scrollToBottom(
+  ) {
+      outputField.scrollTop = outputField.scrollHeight;
     }
   });
-
-  sendButton.addEventListener('click', function () {
-    transmitData();
-  });
-
-  function transmitData() {
-    if (inputField.value.length > 0) {
-      outputField.innerText = inputField.value;
-      outputField.style.textAlign = 'left';
-      outputField.style.justifyContent = 'flex-start';
-      outputField.style.alignItems = 'flex-start';
-    } else {
-      outputField.innerText = 'Hello World!';
-      outputField.style.textAlign = 'center';
-      outputField.style.justifyContent = 'center';
-      outputField.style.alignItems = 'center';
-    }
-
-    inputField.value = '';
-    sendButton.style.display = 'none';
-  }
-});
-
-
+  
