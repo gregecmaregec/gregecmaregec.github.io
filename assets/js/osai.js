647f11d5462aab21a8a9f66a4b380e1d1d7215f5
gregor mihelac <alternativa.rocks@gmail.com>
2024-03-08 17:20:39 +0100

    updating osai

--- assets/js/osai.js
+++ assets/js/osai.js
@@ -1,48 +1,57 @@
-document.addEventListener('DOMContentLoaded', function (
-  ) {
-    const inputField = document.getElementById('inputField');
-    const outputField = document.getElementById('outputField');
-    const sendButton = document.getElementById('sendButton');
-  
-    outputField.innerText = 'Hello World!';
-  
-    inputField.addEventListener('input', function (
-  ) {
-      if (this.value.length > 0) {
-        sendButton.style.display = 'block';
-      } else {
-        sendButton.style.display = 'none';
-      }
-    });
-  
-    inputField.addEventListener('keypress', function (e) {
-      if (e.key === 'Enter') {
-        transmitData();
-      }
-    });
-  
-    sendButton.addEventListener('click', function (
-  ) {
-      transmitData();
-    });
-  
-    function transmitData(
-  ) {
-      if (inputField.value.length > 0) {
-        let newContent = document.createElement('div');
-        newContent.textContent = inputField.value;
-        newContent.classList.add('message');
-        outputField.appendChild(newContent);
-        scrollToBottom();
-      }
-  
-      inputField.value = '';
+document.addEventListener('DOMContentLoaded', function () {
+  const inputField = document.getElementById('inputField');
+  const outputField = document.getElementById('outputField');
+  const sendButton = document.getElementById('sendButton');
+
+  outputField.innerText = 'Hello World!';
+
+  inputField.addEventListener('input', function () {
+    if (this.value.length > 0) {
+      sendButton.style.display = 'block';
+    } else {
       sendButton.style.display = 'none';
     }
-  
-    function scrollToBottom(
-  ) {
-      outputField.scrollTop = outputField.scrollHeight;
+  });
+
+  inputField.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      transmitData();
     }
   });
-  
+
+  sendButton.addEventListener('click', function () {
+    transmitData();
+  });
+
+  function transmitData() {
+    if (inputField.value.length > 0) {
+      let newContent = document.createElement('div');
+      newContent.textContent = 'Sending request...';
+      newContent.classList.add('message');
+      outputField.appendChild(newContent);
+      scrollToBottom();
+
+      fetch('https://gmserver.xyz', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'text/plain'
+        },
+        body: inputField.value
+      })
+      .then(response => response.text())
+      .then(data => {
+        newContent.textContent = data;
+      })
+      .catch(error => {
+        newContent.textContent = 'Error: ' + error.message;
+      });
+    }
+
+    inputField.value = '';
+    sendButton.style.display = 'none';
+  }
+
+  function scrollToBottom() {
+    outputField.scrollTop = outputField.scrollHeight;
+  }
+});
