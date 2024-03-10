7de7a5fa3535d8031a09d0423a282cbb2a55f0d7
gregor mihelac <alternativa.rocks@gmail.com>
2024-03-10 23:50:03 +0100

    remaking it without color

--- assets/js/osai.js
+++ assets/js/osai.js
@@ -35,7 +35,7 @@ document.getElementById('inputBox').addEventListener('keypress', function(event)
         .then(response => response.text())
         .then(responseData => {
             // Display the server's response in the output box
-            outputBox.innerHTML += '<span style="color: rgba(139, 0, 0, 0.8)">' + selectedModel.charAt(0).toUpperCase() + selectedModel.slice(1) + '</span>: ' + '<br>' + responseData + '<br><br>';
+            outputBox.innerHTML += selectedModel.charAt(0).toUpperCase() + selectedModel.slice(1) + ': ' + '<br>' + responseData + '<br><br>';
         })
         .catch(error => {
             // Display the error in the output box
