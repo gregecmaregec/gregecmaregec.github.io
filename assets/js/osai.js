8107e43cec2698928312e8fa57d61dcbb5449a96
gregor mihelac <alternativa.rocks@gmail.com>
2024-03-10 16:03:13 +0100

    more updates

--- assets/js/osai.js
+++ assets/js/osai.js
@@ -1,9 +1,15 @@
+document.getElementById('inputBox').addEventListener('input', function(event) {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+});
+
 document.getElementById('inputBox').addEventListener('keypress', function(event) {
     if (event.key === 'Enter' && !event.shiftKey) {
         event.preventDefault();
         var inputText = this.value;
         var outputBox = document.getElementById('outputBox');
         outputBox.textContent += inputText + '\n';
-        this.value = ''; // Clear input box after pressing Enter
+        this.value = '';
+        this.style.height = '50px'; // Reset height after submission
     }
 });
