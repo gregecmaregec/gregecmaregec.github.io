2745c77bf8a7d793c1c48a463acbb3b89ec178d7
gregor mihelac <alternativa.rocks@gmail.com>
2024-04-09 08:15:42 +0200

    simplifying text input box

--- assets/js/osai_text_input_box.js
+++ assets/js/osai_text_input_box.js
@@ -1,29 +1,19 @@
+
 const inputBox = document.getElementById('inputBox');
 
 function adjustHeight() {
-    const singleLineHeight = 50; // Height for a single line of text
-    const multiLineHeight = 68; // Height for multiple lines of text
-
-    inputBox.style.height = 'auto';
-    const contentHeight = inputBox.scrollHeight;
-
-    if (contentHeight > singleLineHeight) {
-        // If content height exceeds single line height, set to multiLineHeight
-        inputBox.style.height = `${multiLineHeight}px`;
-    } else {
-        // If content fits in a single line, set height to singleLineHeight
-        inputBox.style.height = `${singleLineHeight}px`;
-    }
+inputBox.style.height = 'auto';
+inputBox.style.height = `${inputBox.scrollHeight}px`;
 }
 
 inputBox.addEventListener('input', adjustHeight);
 inputBox.addEventListener('keydown', (event) => {
-    if (event.shiftKey && event.code === 'Enter') {
-        event.preventDefault();
-        const start = inputBox.selectionStart;
-        const end = inputBox.selectionEnd;
-        inputBox.value = inputBox.value.substring(0, start) + '\n' + inputBox.value.substring(end);
-        inputBox.selectionStart = inputBox.selectionEnd = start + 1;
-        adjustHeight();
-    }
+if (event.shiftKey && event.code === 'Enter') {
+    event.preventDefault();
+    const start = inputBox.selectionStart;
+    const end = inputBox.selectionEnd;
+    inputBox.value = inputBox.value.substring(0, start) + '\n' + inputBox.value.substring(end);
+    inputBox.selectionStart = inputBox.selectionEnd = start + 1;
+    adjustHeight();
+}
 });
