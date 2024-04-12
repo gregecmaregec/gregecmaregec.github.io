9b8dcd5bb528b59b16d2d1077fe4deac05a9d1c2
gregor mihelac <alternativa.rocks@gmail.com>
2024-04-13 00:35:48 +0200

    updating

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,6 +1,6 @@
 function adjustFooterPosition() {
     var footer = document.getElementById('footer');
-    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
     var footerHeight = footer.offsetHeight;
     var viewportHeight = window.innerHeight;
     var documentHeight = document.documentElement.scrollHeight;
