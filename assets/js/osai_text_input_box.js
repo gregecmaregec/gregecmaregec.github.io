aa13ddaf4e43b923cb4ce351d276a8e65aef55d2
Gregor Mihelac <123545531+gregecmaregec@users.noreply.github.com>
2024-04-12 11:02:24 +0200

    Update osai_text_input_box.js

--- assets/js/osai_text_input_box.js
+++ assets/js/osai_text_input_box.js
@@ -1,14 +1,25 @@
 const inputBox = document.getElementById('inputBox');
 inputBox.style.height = '50px'; // Set the initial height to 50px
+let isExpanded = false; // Flag to track if the input box has been expanded
 
 function adjustHeight() {
   const scrollHeight = inputBox.scrollHeight;
   const boxHeight = inputBox.offsetHeight;
 
   if (scrollHeight > boxHeight) {
-    inputBox.style.height = `${scrollHeight}px`;
+    // Content is overflowing
+    if (!isExpanded) {
+      // Expand the input box only if it hasn't been expanded before
+      inputBox.style.height = `${scrollHeight}px`;
+      isExpanded = true; // Set the flag to true
+    }
   } else {
-    inputBox.style.height = '50px'; // Reset to the default height if no overflow
+    // Content is not overflowing
+    if (isExpanded) {
+      // Reset the input box height only if it was previously expanded
+      inputBox.style.height = '50px';
+      isExpanded = false; // Reset the flag to false
+    }
   }
 }
 
