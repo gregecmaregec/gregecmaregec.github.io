a95f843db3d4ad62fc96d1f3de589fec7af71494
gregecmaregec <alternativa.rocks@gmail.com>
2024-07-31 00:31:08 +0200

    back to 800 400 BAYBEEEE

--- assets/js/nature.js
+++ assets/js/nature.js
@@ -4,8 +4,8 @@ document.body.appendChild(canvas);
 const ctx = canvas.getContext('2d');
 
 // Define the target size
-const TARGET_WIDTH = 840;
-const TARGET_HEIGHT = 350;
+const TARGET_WIDTH = 800;
+const TARGET_HEIGHT = 400;
 
 // Function to set canvas size
 function setCanvasSize() {
