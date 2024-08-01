0bff4a6765effb42a7c4cb55ab58f1b0a2c4eeb7
gregecmaregec <123545531+gregecmaregec@users.noreply.github.com>
2024-08-01 10:17:32 +0000

    Deploying to gh-pages from @ gregecmaregec/gregecmaregec.github.io@7f34eaa0251b98981bc4d2656ee8e9f11b1e4834 🚀

--- python.py
+++ python.py
@@ -1,7 +1,7 @@
 # this is for me to run python scripts for myself basically lmao
 
-TARGET_WIDTH = 820*1.15
-TARGET_HEIGHT = 300*1.15
+TARGET_WIDTH = 820*1.05
+TARGET_HEIGHT = 300*1.05
 
 x = TARGET_WIDTH
 y = TARGET_HEIGHT
