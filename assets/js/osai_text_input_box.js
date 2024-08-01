ff557a570452ed6979e3ab24441422648f28f26c
gregecmaregec <alternativa.rocks@gmail.com>
2024-08-01 17:48:21 +0200

    some new things

--- assets/js/osai_text_input_box.js
+++ assets/js/osai_text_input_box.js
@@ -2,33 +2,27 @@ const inputBox = document.getElementById('inputBox');
 const initialHeight = 50; // Set the initial height in pixels
 inputBox.style.height = `${initialHeight}px`;
 
-let lastLine = '';
-let isOverflown = false;
+let isExpanded = false;
 
 function adjustHeight() {
-  const lines = inputBox.value.split('\n');
-  const currentLine = lines[lines.length - 1];
   const currentScrollHeight = inputBox.scrollHeight;
+  const lines = inputBox.value.split('\n');
 
-  if (currentScrollHeight > initialHeight) {
-    if (currentLine.length > lastLine.length || isOverflown) {
-      // New character added or already overflown
-      inputBox.style.height = 'auto';
-      const scrollHeight = inputBox.scrollHeight;
-      inputBox.style.height = `${scrollHeight}px`;
-      isOverflown = true;
-    }
-  } else if (currentLine.length < lastLine.length && !isOverflown) {
-    // Character removed and not overflown
+  if (lines.length > 1 || currentScrollHeight > initialHeight) {
+    inputBox.style.height = 'auto';
+    inputBox.style.height = `${inputBox.scrollHeight}px`;
+    isExpanded = true;
+  } else if (isExpanded && lines.length === 1 && currentScrollHeight <= initialHeight) {
     inputBox.style.height = `${initialHeight}px`;
+    isExpanded = false;
   }
-
-  lastLine = currentLine;
 }
 
 inputBox.addEventListener('input', adjustHeight);
 inputBox.addEventListener('keydown', (event) => {
-  if (event.shiftKey && event.code === 'Enter') {
+  if (event.key === 'Enter' && !event.shiftKey) {
+    event.preventDefault(); // Prevent default Enter behavior
+  } else if (event.shiftKey && event.key === 'Enter') {
     event.preventDefault();
     const start = inputBox.selectionStart;
     const end = inputBox.selectionEnd;
