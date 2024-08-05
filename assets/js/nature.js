d1bf07f648cd75c60ef932693770ff8cbbecc8c3
gregecmaregec <alternativa.rocks@gmail.com>
2024-08-05 13:08:50 +0200

    mkaing it slower

--- assets/js/nature.js
+++ assets/js/nature.js
@@ -31,8 +31,8 @@ class Particle {
         this.x = x;
         this.y = y;
         this.size = Math.random() * 3 + 1;
-        this.speedX = (Math.random() * 1 - 0.2) * 0.5;
-        this.speedY = (Math.random() * 1 - 0.2) * 0.5;
+        this.speedX = (Math.random() * 1 - 0.2) * 0.1;
+        this.speedY = (Math.random() * 1 - 0.2) * 0.1;
         this.color = `hsl(${Math.random() * 360}, 70%, 50%)`;
         this.lifespan = 300 + Math.random() * 200;
     }
